d705fb8695b82fcf93fbac2989b0e7a412cdaef2
vada <138933714+Ajvada@users.noreply.github.com>
2023-08-31 20:32:53 -0400

    just 5 left

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,5 +1,6 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -7,7 +8,7 @@ import org.launchcode.techjobs.oo.*;
 import static org.junit.Assert.*;
 
 @RunWith(JUnit4.class)
-public class JobTest {
+public class JobTest extends Job {
     @Test
     public void testSettingJobId() {
         Job job1 = new Job();
@@ -38,4 +39,47 @@ public class JobTest {
     Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
     assertFalse(job == job2);
     }
+    @Test
+    public void testToStringStartsAndEndsWithNewLine() {
+        Employer launchCode = new Employer("launchCode");
+        Location stl = new Location("stl");
+        PositionType BackendDeveloper = new PositionType("Back-end developer");
+        CoreCompetency Java = new CoreCompetency("Java");
+        Job job = new Job("Web Developer", launchCode, stl, BackendDeveloper, Java);
+//        Job job = new Job();
+        String expectedOutput = "\n" +
+                "ID: _______\n" +
+                "Name: _______\n" +
+                "Employer: _______\n" +
+                "Location: _______\n" +
+                "Position Type: _______\n" +
+                "Core Competency: _______\n" +
+                "\n";
+        char firstChar = job.toString().charAt(0);
+        char lastChar = job.toString().charAt(job.toString().length()-1);
+
+        String actualOutput = job.toString();
+        Assert.assertEquals(expectedOutput, actualOutput);
+        Assert.assertEquals('\n', '\n');
+        Assert.assertEquals('\n', '\n');
+        Assert.assertEquals( '\n',firstChar);
+        Assert.assertEquals('\n', lastChar);
+
+        String jobToString = job.toString();
+        assertTrue(jobToString.startsWith("\n"));
+        assertTrue(jobToString.endsWith("\n"));
+    }
+    @Test
+    public void testToStringStartsAndEndsWithNewLine2() {
+        Employer launchCode = new Employer("LaunchCode");
+        Location stl = new Location("StL");
+        PositionType backendDeveloper = new PositionType("Back-end developer");
+        CoreCompetency java = new CoreCompetency("Java");
+        Job job = new Job("Web Developer", launchCode, stl, backendDeveloper, java);
+
+        String jobToString = job.toString();
+        Assert.assertTrue(jobToString.startsWith("\n"));
+        Assert.assertTrue(jobToString.endsWith("\n"));
+    }
+
 }
